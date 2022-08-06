50dcf02f104b58849c4e9f8098f16979ca4dfd9f
Louis Legrain <contact@louislegrain.online>
2022-08-06 10:39:53 +0200

    Code optimizations and minor changes

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-const menu = document.querySelector('.menu')
-            const close = document.querySelector('.close')
-            const nav = document.querySelector('nav')
+const toggleBtn = document.getElementById('toggle-menu');
 
-            menu.addEventListener('click', () => {
-                nav.classList.add('open-nav')
-            })
+let hashChange = 0;
 
-            close.addEventListener('click', () => {
-                nav.classList.remove('open-nav')
-            })
+window.addEventListener('hashchange', e => {
+   if (hashChange !== 0) {
+      if (hashChange === 2) hashChange = 0;
+      return;
+   }
+   e.stopImmediatePropagation();
+   hashChange = 1;
+   const hash = window.location.hash;
+   window.location.hash = new URL(e.oldURL).hash;
+   toggleBtn.checked = false;
+   setTimeout(() => {
+      window.location.hash = hash;
+      hashChange = 2;
+      if (window.matchMedia('screen and (max-width: 768px)').matches)
+         document
+            .querySelector(`.section[data-anchor="${hash.replace(/^#/, '')}"]`)
+            ?.scrollIntoView({ behavior: 'smooth' });
+   }, 120);
+});
 
 new fullpage('#fullpage', {
-    navigation: true,
-    responsiveWidth: 700,
-    anchors: ['section1', 'section2', 'section3', 'section4'],
-    parallax: true,
-    onLeave: function(origin, destination, direction){
-        console.log("Leaving section" + origin.index);
-    },
+   navigation: true,
+   responsiveWidth: 768,
+});
+
+toggleBtn.addEventListener('change', e => {
+   if (e.target.checked) document.documentElement.scrollTop = 0;
+   document.documentElement.style.overflowY = e.target.checked ? 'hidden' : 'scroll';
 });
